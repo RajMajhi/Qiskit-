96e753ac350ecbd78591d9b2c8ec9e09a4ebaed1
Raj Majhi <rajm46921@gmail.com>
2026-01-30 12:37:19 +0530

    Update QC.py

--- QC.py
+++ QC.py
@@ -97,4 +97,97 @@ qc.draw()
 
 #results: ('11': 1)
 
+#switch case
+from qiskit import QuantumRegister, ClassicalRegister
 
+qreg = QuantumRegister(2)
+creg = ClassicalRegister(2)
+qc = QuantumCircuit(qreg, creg)
+qc.h ([0, 1])
+qc.measure([0, 1],[0, 1])
+
+with qc.switch(creg) as case:
+    with case(0):
+        qc.z(0)
+    with case(1, 2):
+        qc.cs(0, 1)
+    with case(case.DEFAULT):
+        qc.h(0)
+
+qc.draw()
+
+!pip install matplotlib pylatexenc
+
+from qiskit import QuantumCircuit
+from IPython.display import display
+
+qc = QuantumCircuit(1, 1)
+qc.measure(0, 0)
+print(qc.draw('text'))
+
+import qiskit 
+print(qiskit.__version__)
+
+import sys
+!{sys.executable} -m pip install qiskit-aer
+
+from qiskit_aer import Aer
+from qiskit import transpile
+
+backend = Aer.get_backend('qasm_simulator')
+
+compiled = transpile(qc, backend)
+
+job= backend.run(compiled, shots=1000)
+result = job.result()
+counts = result.get_counts()
+print(counts)
+
+#hadamard gate (H) 
+'''
+|0> -> (|0> + |1>)/√2 
+'''
+qc = QuantumCircuit(1, 1)
+qc.h(0)
+qc.measure(0, 0)
+backend = Aer.get_backend('qasm_simulator')
+compiled = transpile(qc, backend)
+job = backend.run(compiled, shots=1000)
+counts = job.result().get_counts()
+print(counts) # 50% 0, 50% 1 -> superposition
+
+from qiskit.visualization import plot_circuit_layout
+qc.draw('text')
+
+from qiskit.visualization import plot_circuit_layout
+qc.draw('mpl')
+
+# Multiple Qubits
+qc= QuantumCircuit(2, 2)
+qc.draw('mpl')
+
+#CNOT gate (Entanglement starts here)
+'''
+Control qubit controls target qubit
+if control = 1 target flips
+'''
+qc = QuantumCircuit(2, 2)
+
+qc.h(0)        # Put qubit 0 in superposition
+qc.cx(0, 1)    # CNOT (control=0, target=1)
+
+qc.measure([0,1], [0,1])
+
+backend = Aer.get_backend('qasm_simulator')
+compiled = transpile(qc, backend)
+job = backend.run(compiled, shots=1000)
+
+print(job.result().get_counts())
+
+from qiskit import QuantumCircuit
+qc= QuantumCircuit(2, 2)
+qc.x(1)
+qc.measure([0,1],[0,1])
+qc.draw('mpl')
+
+print(qc.draw('text'))
